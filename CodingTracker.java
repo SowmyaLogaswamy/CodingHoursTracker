aab597835a7f3c6d68720d55a676cf553a9bef84
Esvethlynna Pangelinan and Sowmya Logaswamy <esvethlynna@>
2017-06-26 09:14:27 -0700

    Add branching statements to the java program

--- CodingTracker.java
+++ CodingTracker.java
@@ -1,30 +1,17 @@
 import java.io.Console;
-
 public class CodingTracker {
   public static void main(String[] args) {
-
     int totalHours1 = calculateHoursSpent("yesterday");
     int totalHours2 = calculateHoursSpent("day before yesterday");
     int totalHours3 = calculateHoursSpent("today");
-
     int totalHours = totalHours1 + totalHours2 + totalHours3;
     System.out.println("Your spent a total of " + totalHours + " hours coding");
-  //
-  // System.out.println("You have spent " + hoursYesterday + " hours");
-  //
-  // System.out.println("How many hours did you spend coding day before yesterday?");
-  // String stringNumber2 = myConsole.readLine();
-  // Integer hoursDayBeforeYesterday = Integer.parseInt(stringNumber2);
-  // System.out.println("You have spent " + hoursDayBeforeYesterday + " hours");
-  //
-  // System.out.println("How many hours did you spend coding today?");
-  // String stringNumber3 = myConsole.readLine();
-  // Integer hoursToday = Integer.parseInt(stringNumber3);
-  // System.out.println("You spent " + hoursToday + " hours.");
-  //
-  // int totalHours = hoursYesterday + hoursDayBeforeYesterday + hoursToday;
-  // System.out.println("You spent a total of " + totalHours + " hours on coding!");
-}
+    if(totalHours > 10) {
+      System.out.println("Great job! You're on track to becoming a Java pro in no time!");
+    } else {
+      System.out.println("Thats Okay!! Keep practicing");
+    }
+  }
 
   public static Integer calculateHoursSpent(String day) {
     Console myConsole = System.console();
@@ -33,6 +20,5 @@ public class CodingTracker {
     Integer hoursSpent = Integer.parseInt(stringNumber);
     System.out.println("You have spent " + hoursSpent + " hours " + day);
     return hoursSpent;
-
   }
 }
